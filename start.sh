071b23e8c9bc76145886b02b78241b3eac5775f2
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-08-08 00:56:41 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
-cd /MusicPlayer
+git clone https://github.com/subinps/VCPvtBot.git /VCPvtBot
+cd /VCPvtBot
 echo "Starting Bot...."
 python3 main.py
